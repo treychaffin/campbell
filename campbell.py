ecf6713dfacaed95b3c273dda566d19be3146f5d
treychaffin <treychaffin@wildcatfabrication.com>
2024-07-21 15:57:14 -0500

    development of campbell class

--- campbell.py
+++ campbell.py
@@ -1,10 +1,13 @@
 import requests
 
-class campbell():
-    def __init__(self, address) -> None:
-        self.address = address
 
-    def print_latest_data(self, table) -> None:
+class campbell:
+    def __init__(self, address: str, tables: list) -> None:
+        self.address: str = address
+        self.tables: list = tables
+        self.datafields: dict = self._datafields()
+
+    def _fields(self, table) -> list:
         command = "dataquery"
         format = "json"
         mode = "backfill"
@@ -14,23 +17,50 @@ class campbell():
 
         response = requests.get(url)
 
+        fields: list = []
+
         if response.status_code == 200:
             data = response.json()
-            date, time = data["data"][0]["time"].split("T")
-            print(f"Date:\t\t{date}")
-            print(f"Time:\t\t{time}\tUTC")
             for i in range(len(data["head"]["fields"])):
-                field = data["head"]["fields"][i]['name']
-                value = data["data"][1]["vals"][i]
-
-                try: 
-                    units = data["head"]["fields"][i]["units"]
-                except KeyError:
-                    units = ""
-
-                if len(field) > 7:
-                    print(f"{field}\t{value}\t\t{units}")
-                else:
-                    print(f"{field}\t\t{value}\t\t{units}")
+                fields.append(data["head"]["fields"][i]["name"])
         else:
             print(f"Failed to retrieve data: {response.status_code}")
+
+        return fields
+
+    def _datafields(self) -> dict:
+        datafields: dict = {}
+        for table in range(len(self.tables)):
+            datafields[self.tables[table]] = self._fields(self.tables[table])
+        return datafields
+
+    # def print_latest_data(self, table) -> None:
+    #     command = "dataquery"
+    #     format = "json"
+    #     mode = "backfill"
+    #     parameter = "p1=1"
+
+    #     url = f"http://{self.address}/?command={command}&uri=dl:{table}&format={format}&mode={mode}&{parameter}"
+
+    #     response = requests.get(url)
+
+    #     if response.status_code == 200:
+    #         data = response.json()
+    #         date, time = data["data"][0]["time"].split("T")
+    #         print(f"Date:\t\t{date}")
+    #         print(f"Time:\t\t{time}\tUTC")
+    #         for i in range(len(data["head"]["fields"])):
+    #             field = data["head"]["fields"][i]["name"]
+    #             value = data["data"][1]["vals"][i]
+
+    #             try:
+    #                 units = data["head"]["fields"][i]["units"]
+    #             except KeyError:
+    #                 units = ""
+
+    #             if len(field) > 7:
+    #                 print(f"{field}\t{value}\t\t{units}")
+    #             else:
+    #                 print(f"{field}\t\t{value}\t\t{units}")
+    #     else:
+    #         print(f"Failed to retrieve data: {response.status_code}")
