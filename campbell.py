3f408fd5f0a31939e34af5f0d492809af6e88ba7
treychaffin <trey@wildcat-fab.com>
2024-12-11 14:16:38 -0600

    modified library for the basic functionality defined in the API

--- campbell.py
+++ campbell.py
@@ -1,95 +1,167 @@
 import requests
+import datetime
+from typing import Union
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 class campbell:
-    """Used to interface with Campbell Scientific data loggers"""
+    """
+    Used to interface with Campbell Scientific data loggers via the web API
 
-    def __init__(self, address: str, tables: list) -> None:
+    https://help.campbellsci.com/crbasic/cr1000x/Content/Info/webserverapicommands1.htm
+    """
+
+    def __init__(
+        self,
+        address: str,
+        tables: list,
+        **kwargs,
+    ) -> None:
         """Initializes the class with the address of the data logger and a list of tables"""
         self.address: str = address
         self.tables: list = tables
-        self.datafields: dict = self._datafields()
-        self.data: dict = self.pull_recent()
+        self.time_offset: datetime.timedelta = kwargs.get(
+            "time_offset", datetime.timedelta(hours=0)
+        )
 
-    def _fields(self, table) -> list:
-        """Returns a list of fields for a given table"""
-        command = "dataquery"
-        format = "json"
-        mode = "backfill"
-        parameter = "p1=1"
+        # list acceptable formats as defined in API documentation
+        self.formats: list = [
+            "html",
+            "json",
+            "toa5",
+            "tob1",
+            "xml",
+        ]
 
+    def _request(
+        self, command: str, table: str, format: str, mode: str, parameter: str
+    ) -> requests.Response:
+        """returns the response from the data logger"""
         url = f"http://{self.address}/?command={command}&uri=dl:{table}&format={format}&mode={mode}&{parameter}"
-
         response = requests.get(url)
-
-        fields: list = []
-
         if response.status_code == 200:
-            data = response.json()
-            for i in range(len(data["head"]["fields"])):
-                fields.append(data["head"]["fields"][i]["name"])
+            return response
         else:
-            print(f"Failed to retrieve data: {response.status_code}")
+            logging.error(
+                f"Request failed with status code: {response.status_code} url: {url}"
+            )
+            response.raise_for_status()
+            return response
 
-        return fields
-
-    def _datafields(self) -> dict:
-        """Returns a dictionary of fields for all tables"""
-        datafields: dict = {}
-        for table in range(len(self.tables)):
-            datafields[self.tables[table]] = self._fields(self.tables[table])
-        return datafields
-
-    def pull_recent(self) -> dict:
-        """Pulls the most recent data"""
+    def _data(self, command: str, format: str, mode: str, parameter: str) -> dict:
+        """returns the data from the response"""
 
         data: dict = {}
 
-        command = "dataquery"
-        format = "json"
-        mode = "backfill"
-        parameter = "p1=1"
-
         for table in self.tables:
-            url = f"http://{self.address}/?command={command}&uri=dl:{table}&format={format}&mode={mode}&{parameter}"
-
-            response = requests.get(url)
-
-            if response.status_code == 200:
-                recent_data = response.json()
-                data[table] = recent_data
+            if format == "json":
+                data[table] = self._request(
+                    command, table, format, mode, parameter
+                ).json()
             else:
-                print(f"Failed to retrieve data: {response.status_code}")
+                data[table] = self._request(
+                    command, table, format, mode, parameter
+                ).text
 
         return data
 
-    def data_table(self, table) -> list:
-        """Prints the data table"""
-        time = self.data[table]["data"][0]["time"]
-        data_table = [["time", time, "UTC"]]
+    def _assert_format(self, format: str) -> None:
+        assert format in self.formats, f"format must be one of {self.formats}"
 
-        for field in range(len(self.data[table]["head"]["fields"])):
-            name = self.data[table]["head"]["fields"][field]["name"]
-            value = self.data[table]["data"][0]["vals"][field]
-            try:
-                units = self.data[table]["head"]["fields"][field]["units"]
-            except KeyError:
-                units = ""
-            data_table.append([name, value, units])
+    def most_recent(self, records: int = 1, **kwargs) -> dict:
+        """
+        Returns the data from the most recent number of records
 
-        return data_table
+        Parameters:
+            records (int): the number of records to pull
+        """
 
-    def data_dict(self, table) -> dict:
-        """Returns a dictionary of the most recent data"""
-        data_dict = {}
-        time = self.data[table]["data"][0]["time"]
-        data_dict["datetime"] = [time, "UTC"]
-        for field in range(len(self.data[table]["head"]["fields"])):
-            name = self.data[table]["head"]["fields"][field]["name"]
-            value = self.data[table]["data"][0]["vals"][field]
-            try:
-                units = self.data[table]["head"]["fields"][field]["units"]
-            except KeyError:
-                units = ""
-            data_dict[name] = [value, units]
-        return data_dict
+        command = "dataquery"
+        format = kwargs.get("format", "json")
+        self._assert_format(format)
+        mode = "backfill"
+        parameter = f"p1={records}"
+
+        return self._data(command, format, mode, parameter)
+
+    def since_time(self, time: datetime.timedelta, **kwargs) -> dict:
+        """
+        Returns all the data since a certain time
+
+        Parameters:
+            time (datetime.timedelta): the time to pull data since
+
+        Returns:
+            dict: a dictionary of the data since the time
+        """
+
+        now = datetime.datetime.now()
+        since_time = now - time - self.time_offset
+
+        command = "dataquery"
+        format = kwargs.get("format", "json")
+        self._assert_format(format)
+        mode = "since-time"
+        parameter = f"p1={since_time.strftime("%Y-%m-%dT%H:%M:%S.%f")}"
+
+        return self._data(command, format, mode, parameter)
+
+    def since_record(self, record: int, **kwargs) -> dict:
+        """
+        Returns all the data since a certain record
+
+        Parameters:
+            record (int): the record to pull data since
+
+        Returns:
+            dict: a dictionary of the data since the record
+        """
+
+        command = "dataquery"
+        format = kwargs.get("format", "json")
+        self._assert_format(format)
+        mode = "since-record"
+        parameter = f"p1={record}"
+
+        return self._data(command, format, mode, parameter)
+
+    def date_range(
+        self, start: datetime.datetime, end: datetime.datetime, **kwargs
+    ) -> dict:
+        """
+        Returns all the data between two dates
+
+        Parameters:
+            start (datetime.datetime): the start date
+            end (datetime.datetime): the end date
+
+        Returns:
+            dict: a dictionary of the data between the two dates
+        """
+
+        command = "dataquery"
+        format = kwargs.get("format", "json")
+        self._assert_format(format)
+        mode = "date-range"
+        parameter = f"p1={start.strftime("%Y-%m-%dT%H:%M:%S.%f")}&p2={end.strftime("%Y-%m-%dT%H:%M:%S.%f")}"
+
+        return self._data(command, format, mode, parameter)
+
+    def backfill(self, interval: datetime.timedelta, **kwargs) -> dict:
+        """
+        Returns the data from the called interval
+
+        Parameters:
+            interval (datetime.timedelta): the interval to pull data from
+        """
+
+        command = "dataquery"
+        format = kwargs.get("format", "json")
+        self._assert_format(format)
+        mode = "backfill"
+        parameter = f"p1={int(interval.total_seconds())}"
+
+        return self._data(command, format, mode, parameter)
--- testing.py
+++
@@ -1,5 +0,0 @@
-from campbell import campbell
-
-ASSET = campbell("172.17.204.40")
-
-ASSET.print_latest_data("ASSET_1min")
