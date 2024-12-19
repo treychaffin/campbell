ca80272a1aa08bf9c3c0f725992a61f9f7b8cee3
treychaffin <treychaffin@gmail.com>
2024-12-19 21:41:51 +0000

    improvements

--- campbell.py
+++ campbell.py
@@ -1,11 +1,12 @@
 import requests
 import datetime
 import base64
+import time
 from typing import Union
-
+from urllib.parse import quote
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
 
 
 class Campbell:
@@ -15,29 +16,43 @@ class Campbell:
     https://help.campbellsci.com/crbasic/cr1000x/Content/Info/webserverapicommands1.htm
     """
 
-    def __init__(
-        self,
-        address: str,
-        tables: list,
-        **kwargs,
-    ) -> None:
+    def __init__(self, address: str, **kwargs) -> None:
         """
         Initializes the class with the address of the data logger
         and a list of tables
         """
+
+        start_time = time.time()
         self.address: str = address
-        self.tables: list = tables
+
         self.timeout = kwargs.get("timeout", 10)
         if "username" in kwargs and "password" in kwargs:
             self.username = kwargs["username"]
-            self.password = kwargs["password"]
+            self.password = quote(kwargs["password"])
 
         self.DataAccess = self._DataAccess(self)
+        self.Data = self.DataAccess  # alias
         self.ControlCommand = self._ControlCommand(self)
+        self.Control = self.ControlCommand  # alias
         self.FileManagement = self._FileManagement(self)
+        self.File = self.FileManagement  # alias
 
-    # def _api_request(self, command: str, **kwargs) -> Union[dict, str]:
-    def _api_request(self, command: str, **kwargs) -> None:
+        if "tables" in kwargs:
+            self.tables = kwargs["tables"]
+        else:
+            self.tables = self._get_tables()
+
+        end_time = time.time()
+        duration = end_time - start_time
+        log.info(f"Initialization took {duration:.6f} seconds")
+
+    def _get_tables(self) -> list:
+        tables = []
+        for name in self.Data.symbols()['symbols']:
+            tables.append(name['name'])
+        return tables
+
+    def _api_request(self, command: str, **kwargs) -> Union[dict, str, None]:
 
         if hasattr(self, "username") and hasattr(self, "password"):
             url = f"http://{self.username}:{self.password}@{self.address}/"
@@ -64,6 +79,9 @@ class Campbell:
         if "table" in kwargs:
             url += f"&uri=dl:{kwargs['table']}"
 
+        if "field" in kwargs:
+            url += f".{kwargs['field']}"
+
         if "value" in kwargs:
             url += f"&value={kwargs['value']}"
 
@@ -82,37 +100,34 @@ class Campbell:
         if "parameter2" in kwargs:
             url += f"&p2={kwargs['parameter2']}"
 
-        if "format" in kwargs and kwargs["format"] == "json":
-            # return self._request(url).json()
-            return self._request(url)
-        else:
-            # return self._request(url).text
-            return self._request(url)
+        log.debug(f"{url=}")
 
-    # def _request(self, url) -> requests.Response:
-    # try:
-    #     response = requests.get(url, timeout=self.timeout)
-    #     if response.status_code == 200:
-    #         return response
-    #     response.raise_for_status()
+        response = self._request(url)
+        if response.status_code == 200:
+            if "format" in kwargs and kwargs["format"] == "json":
+                return self._request(url).json()
+            else:
+                return self._request(url).text
 
-    # except requests.exceptions.HTTPError as errh:
-    #     logging.error("Http Error:", errh)
+    def _request(self, url) -> requests.Response:
+        try:
+            response = requests.get(url, timeout=self.timeout)
+            response.raise_for_status()
 
-    # except requests.exceptions.ConnectionError as errc:
-    #     logging.error("Error Connecting:", errc)
+        except requests.exceptions.HTTPError as errh:
+            log.error(f"Http Error: {errh}")
 
-    # except requests.exceptions.Timeout as errt:
-    #     logging.error("Timeout Error:", errt)
+        except requests.exceptions.ConnectionError as errc:
+            log.error(f"Error Connecting: {errc}")
 
-    # except requests.exceptions.RequestException as err:
-    #     logging.error("OOps: Something Else", err)
+        except requests.exceptions.Timeout as errt:
+            log.error(f"Timeout Error: {errt}")
 
-    # finally:
-    #     return response
+        except requests.exceptions.RequestException as err:
+            log.error(f"Error: {err}")
 
-    def _request(self, url) -> None:
-        print(url)
+        finally:
+            return response
 
     class _DataAccess:
         def __init__(self, parent):
@@ -138,6 +153,7 @@ class Campbell:
                 "end",
                 "interval",
                 "table",
+                "field",
             ]
             for key in kwargs:
                 assert key in valid_kwargs, f"invalid keyword argument: {key}"
@@ -148,28 +164,36 @@ class Campbell:
                     kwargs["format"] in valid_formats
                 ), f"format must be one of {valid_formats}"
 
-            format = kwargs.get("format", "json")  # default to json
-
             def _get_data(**kwargs):
                 data = {}
-
-                for table in self.parent.tables:
-                    data[table] = self.parent._api_request(command, **kwargs)
+                if "table" in kwargs:
+                    table = kwargs["table"]
+                    kwargs.pop("table")
+                    data[table] = self.parent._api_request(
+                        command, table=table, **kwargs
+                    )
+                else:
+                    for table in self.parent.tables:
+                        data[table] = self.parent._api_request(
+                            command, table=table, **kwargs)
 
                 return data
 
             if mode == "most-recent":
                 assert "records" in kwargs, "records (int) must be provided"
-                assert isinstance(kwargs["records"], int), "records must be an int"
+                assert isinstance(kwargs["records"],
+                                  int), "records must be an int"
 
                 parameter = kwargs["records"]
 
                 return _get_data(
-                    mode=mode, parameter1=parameter, format=format, **kwargs
+                    mode=mode, parameter1=parameter, **kwargs
                 )
 
             elif mode == "since-time":
-                assert "time" in kwargs, "time (datetime.datetime) must be provided"
+                assert "time" in kwargs, (
+                    "time (datetime.datetime) must be provided"
+                )
                 assert isinstance(
                     kwargs["time"], datetime.datetime
                 ), "time must be a datetime.datetime object"
@@ -177,17 +201,18 @@ class Campbell:
                 parameter = kwargs["time"].strftime("%Y-%m-%dT%H:%M:%S.%f")
 
                 return _get_data(
-                    mode=mode, parameter1=parameter, format=format, **kwargs
+                    mode=mode, parameter1=parameter, **kwargs
                 )
 
             elif mode == "since-record":
                 assert "record" in kwargs, "record (int) must be provided"
-                assert isinstance(kwargs["record"], int), "record must be an int"
+                assert isinstance(kwargs["record"],
+                                  int), "record must be an int"
 
                 parameter = kwargs["record"]
 
                 return _get_data(
-                    mode=mode, parameter=parameter, format=format, **kwargs
+                    mode=mode, parameter1=parameter, **kwargs
                 )
 
             elif mode == "date-range":
@@ -197,9 +222,9 @@ class Campbell:
                         "and end (datetime.datetime) must be provided",
                     ]
                 )
-                assert isinstance(kwargs["start"], datetime.datetime) and isinstance(
-                    kwargs["end"], datetime.datetime
-                ), "start and end must be datetime.datetime objects"
+                assert isinstance(kwargs["start"], datetime.datetime) and \
+                    isinstance(kwargs["end"], datetime.datetime), \
+                    "start and end must be datetime.datetime objects"
 
                 parameter1 = kwargs["start"].strftime("%Y-%m-%dT%H:%M:%S.%f")
                 parameter2 = kwargs["end"].strftime("%Y-%m-%dT%H:%M:%S.%f")
@@ -208,7 +233,6 @@ class Campbell:
                     mode=mode,
                     parameter1=parameter1,
                     parameter2=parameter2,
-                    format=format,
                     **kwargs,
                 )
 
@@ -223,8 +247,10 @@ class Campbell:
                 parameter = int(kwargs["interval"].total_seconds())
 
                 return _get_data(
-                    mode=mode, parameter1=parameter, format=format, **kwargs
+                    mode=mode, parameter1=parameter, **kwargs
                 )
+
+        query = dataquery  # alias
 
         def browsesymbols(self, **kwargs):
             valid_kwargs = ["table", "format"]
@@ -242,6 +268,8 @@ class Campbell:
 
             return self.parent._api_request(command, format=format, **kwargs)
 
+        symbols = browsesymbols  # alias
+
     class _ControlCommand:
         def __init__(self, parent):
             self.parent = parent
@@ -255,14 +283,24 @@ class Campbell:
                     ]
                 ),
                 3: "LoggerNet logon failed (LoggerNet data sources only)",
-                4: "Blocked by LoggerNet security (LoggerNet data sources only)",
+                4: "".join(
+                    [
+                        "Blocked by LoggerNet security ",
+                        "(LoggerNet data sources only)",
+                    ]
+                ),
                 5: "Read only",
                 6: "Invalid table name",
                 7: "Invalid fieldname",
                 8: "Invalid fieldname subscript",
                 9: "Invalid field data type",
                 10: "Datalogger communication failed",
-                11: "Datalogger communication disabled (LoggerNet data sources only)",
+                11: "".join(
+                    [
+                        "Datalogger communication disabled ",
+                        "(LoggerNet data sources only)",
+                    ]
+                ),
                 12: "Blocked by datalogger security",
                 13: "Invalid table definitions (LoggerNet data sources only)",
                 14: "Invalid device name (LoggerNet data sources only)",
@@ -271,15 +309,40 @@ class Campbell:
             self.clock_result_codes = {
                 1: "The clock was checked (successful)",
                 2: "The clock was set",
-                3: "The LoggerNet session failed (LoggerNet data sources only)",
+                3: "".join(
+                    [
+                        "The LoggerNet session failed ",
+                        "(LoggerNet data sources only)",
+                    ]
+                ),
                 4: "Invalid LoggerNet logon (LoggerNet data sources only)",
-                5: "Blocked by LoggerNet security (LoggerNet data sources only)",
-                6: "Communication with the station failed (LoggerNet data sources only)",
-                7: "Communication with the station disabled (LoggerNet data sources only)",
+                5: "".join(
+                    [
+                        "Blocked by LoggerNet security ",
+                        "(LoggerNet data sources only)",
+                    ]
+                ),
+                6: "".join(
+                    [
+                        "Communication with the station failed ",
+                        "(LoggerNet data sources only)",
+                    ]
+                ),
+                7: "".join(
+                    [
+                        "Communication with the station disabled ",
+                        "(LoggerNet data sources only)",
+                    ]
+                ),
                 8: "Blocked by datalogger security",
                 9: "Invalid station name (LoggerNet data sources only)",
                 10: "LoggerNet device is busy (LoggerNet data sources only)",
-                11: "Specified URI does not reference a LoggerNet station (LoggerNet data sources only)",
+                11: "".join(
+                    [
+                        "Specified URI does not reference a LoggerNet ",
+                        "station (LoggerNet data sources only)",
+                    ]
+                ),
             }
 
         def setvaluex(self, table, field, value, **kwargs):
